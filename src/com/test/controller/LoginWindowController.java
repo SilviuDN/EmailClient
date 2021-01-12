7e729242fbac7b448db6d16de74090a25533c350
SilviuDN <42895950+SilviuDN@users.noreply.github.com>
2021-01-12 02:39:00 +0100

    JavaMailSetup

--- src/com/test/controller/LoginWindowController.java
+++ src/com/test/controller/LoginWindowController.java
@@ -1,6 +1,9 @@
 package com.test.controller;
 
+import com.sun.mail.smtp.SMTPOutputStream;
 import com.test.EmailManager;
+import com.test.controller.services.LoginService;
+import com.test.model.EmailAccount;
 import com.test.view.ViewFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,11 +29,34 @@ public class LoginWindowController extends BaseController{
 
     @FXML
     void loginButtonAction() {
+        if (fieldsAreValid()){
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult){
+                case SUCCESS:
+                    System.out.println("login succesfull for " + emailAccount );
+                    return;
+            }
+        };
         System.out.println("loginButtonAction()");
         viewFactory.showMainWindow();
         //close the LoginWindow stage
         Stage loginWindowStage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(loginWindowStage);
+    }
+
+    private boolean fieldsAreValid() {
+        if(emailAddressField.getText().isEmpty()){
+            errorLabel.setText("Please fill email");
+            return false;
+        }
+        if(passwordField.getText().isEmpty()){
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+        return true;
     }
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -3,9 +3,11 @@ module horeaJavaFxEmailClientCourse {
     requires javafx.controls;
     requires javafx.graphics;
     requires javafx.web;
+    requires activation;
+    requires java.mail;
 
     opens com.test;
     opens com.test.view;
     opens com.test.controller;
-//    opens com.test.model;
+    opens com.test.model;
 }
